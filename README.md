c18bb47e7d33fda474f22ca4de8eeb311708d679
Ignacio Mazzara <nachomazzara@gmail.com>
2021-05-19 15:47:26 -0300

    feat: add subgraphs and some dynamics

---
+++ README.md
@@ -0,0 +1,22 @@
+# Blocks info to be used on multicall snapshot strategy
+
+## URLS:
+
+- https://thegraph.com/explorer/subgraph/decentraland/blocks-ethereum-mainnet
+- https://thegraph.com/explorer/subgraph/decentraland/blocks-ethereum-ropsten
+- https://thegraph.com/explorer/subgraph/decentraland/blocks-matic-mainnet
+- https://thegraph.com/explorer/subgraph/decentraland/blocks-matic-mumbai
+
+## Install
+
+```bash
+npm i
+```
+
+## Deploy
+
+```bash
+npm run deploy:mainnet
+npm run deploy:mumbai
+# etc
+```
--- src/mappings/blocks.ts
+++ src/mappings/blocks.ts
@@ -4,23 +4,23 @@ import {
 
 import {
     Block
-} from "../../generated/schema"
+} from "../entities/schema"
 
 export function handleBlock(block: EthereumBlock): void {
     let id = block.hash.toHex()
-    let blockEntity = new Block(id);
-    blockEntity.number = block.number;
-    blockEntity.timestamp = block.timestamp;
-    blockEntity.parentHash = block.parentHash.toHex();
-    blockEntity.author = block.author.toHex();
-    blockEntity.difficulty = block.difficulty;
-    blockEntity.totalDifficulty = block.totalDifficulty;
-    blockEntity.gasUsed = block.gasUsed;
-    blockEntity.gasLimit = block.gasLimit;
-    blockEntity.receiptsRoot = block.receiptsRoot.toHex();
-    blockEntity.transactionsRoot = block.transactionsRoot.toHex();
-    blockEntity.stateRoot = block.stateRoot.toHex();
-    blockEntity.size = block.size;
-    blockEntity.unclesHash = block.unclesHash.toHex();
-    blockEntity.save();
-  }
+    let blockEntity = new Block(id)
+    blockEntity.number = block.number
+    blockEntity.timestamp = block.timestamp
+    blockEntity.parentHash = block.parentHash.toHex()
+    blockEntity.author = block.author.toHex()
+    blockEntity.difficulty = block.difficulty
+    blockEntity.totalDifficulty = block.totalDifficulty
+    blockEntity.gasUsed = block.gasUsed
+    blockEntity.gasLimit = block.gasLimit
+    blockEntity.receiptsRoot = block.receiptsRoot.toHex()
+    blockEntity.transactionsRoot = block.transactionsRoot.toHex()
+    blockEntity.stateRoot = block.stateRoot.toHex()
+    blockEntity.size = block.size
+    blockEntity.unclesHash = block.unclesHash.toHex()
+    blockEntity.save()
+}
